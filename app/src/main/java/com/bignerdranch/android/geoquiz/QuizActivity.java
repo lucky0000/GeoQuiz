f02f97afd17ad8c8350ea8427723ba105366c731
LuckY <qmr108@163.com>
2018-04-16 22:51:25 +0800

    unit3 一题不能多答 评分

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bignerdranch.android.geoquiz.model.Question;
+
+import java.math.BigDecimal;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -18,8 +21,12 @@ public class QuizActivity extends AppCompatActivity {
     private ImageButton btnNext;
     private ImageButton btnPrev;
     private final static String KEY_INDEX = "index";
+    private final static String KEY_COUNT = "count";
+    private final static String KEY_OKCOUNT = "okCount";
+    private final static String KEY_ISSEND = "questionsIsSend";
+
     private static final String TAG = "QuizActivity";
-    private int currentIndex = 0;
+
     private Question[] questions = new Question[]{
             new Question(R.string.question_africa, true),
             new Question(R.string.question_americas, true),
@@ -28,6 +35,15 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_mideast, false),
             new Question(R.string.question_oceans, true),
     };
+
+    private int currentIndex = 0;
+    //对应的问题是否回答 需保存状态
+    private boolean[] questionsIsSend = new boolean[questions.length];
+
+    //答对的问题数 需保存状态
+    private int okCount;
+    //总答题数 需保存状态
+    private int count;
     private TextView txtQuestion;
 
     @Override
@@ -35,6 +51,11 @@ public class QuizActivity extends AppCompatActivity {
         Log.d(TAG, "onSaveInstanceState: " + currentIndex);
         super.onSaveInstanceState(outState);
         outState.putInt(KEY_INDEX, currentIndex);
+
+        outState.putInt(KEY_OKCOUNT, okCount);
+        outState.putInt(KEY_COUNT, count);
+        outState.putBooleanArray(KEY_ISSEND, questionsIsSend);
+
     }
 
     @Override
@@ -99,45 +120,142 @@ public class QuizActivity extends AppCompatActivity {
         btnFalse.setOnClickListener(v -> checkResult(currentIndex, false));
         btnNext.setOnClickListener(v -> next(1));
         btnPrev.setOnClickListener(v -> next(-1));
-
-
         txtQuestion.setOnClickListener(v -> next(1));
 
-        showQuestion(currentIndex);
+//        showQuestion(currentIndex);
+        next(0);
     }
 
+    /**
+     * 显示第几题
+     *
+     * @param in
+     */
     private void next(int in) {
         currentIndex = currentIndex + in;
 
-        if (currentIndex < 0)
-            currentIndex = questions.length - 1;
+//        if (currentIndex < 0)
+//            currentIndex = questions.length - 1;
+
+        pageAction();
 
         int index = currentIndex % questions.length;
         showQuestion(index);
+        //如果答过题 则隐藏选项按钮
+        checkShowQuestionButton(index);
     }
 
+    /**
+     * 页码处理
+     */
+    private void pageAction() {
+        if (currentIndex <= 0) {
+            currentIndex = 0;
+            btnPrev.setVisibility(View.GONE);
+        } else
+            btnPrev.setVisibility(View.VISIBLE);
+
+        if (currentIndex >= questions.length - 1) {
+            currentIndex = questions.length - 1;
+            btnNext.setVisibility(View.GONE);
+        } else
+            btnNext.setVisibility(View.VISIBLE);
+
+    }
+
+    /**
+     * 显示题目
+     *
+     * @param index
+     */
     private void showQuestion(int index) {
         index = index % questions.length;
-
         txtQuestion.setText(questions[index].getTextResId());
     }
 
-    private void checkResult(int index, boolean isTrue) {
-        index = index % questions.length;
-
-        if (questions[index].isAnswerTrue() == isTrue) {
-            show(R.string.correct_toast);
-            next(1);
-        } else {
-            show(R.string.incorrect_toast);
+    /**
+     * 答题并记录
+     *
+     * @param index
+     * @param isOk
+     */
+    private void questionSend(int index, boolean isOk) {
+        questionsIsSend[index] = true;
+        if (isOk) {
+            okCount++;
+        }
+        count++;
+        if (count == questions.length) {
+            //显示分数
+            double score = (double) okCount / questions.length * 100;
+            show(String.format(getResources().getString(R.string.score_toast), score));
+            restart();
         }
     }
 
+
+    /**
+     * 是否显示题目答题按钮
+     *
+     * @param index
+     */
+    private void checkShowQuestionButton(int index) {
+        if (questionsIsSend[index]) {
+            btnTrue.setVisibility(View.GONE);
+            btnFalse.setVisibility(View.GONE);
+        } else {
+            btnTrue.setVisibility(View.VISIBLE);
+            btnFalse.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * 判断答题是否正确
+     *
+     * @param index
+     * @param isTrue
+     */
+    private void checkResult(int index, boolean isTrue) {
+        index = index % questions.length;
+        boolean result = false;
+        if (questions[index].isAnswerTrue() == isTrue) {
+            result = true;
+            show(R.string.correct_toast);
+        } else {
+            show(R.string.incorrect_toast);
+        }
+
+        questionSend(index, result);
+        next(1);
+    }
+
+
+
+    /**
+     * 答完所有题后重置
+     */
+    private void restart() {
+        currentIndex = 0;
+        questionsIsSend = new boolean[questions.length];
+        okCount = 0;
+        count = 0;
+    }
+
+    /**
+     * 显示提示信息
+     *
+     * @param res
+     */
     private void show(int res) {
         Toast toast = Toast.makeText(this, res, Toast.LENGTH_SHORT);
         toast.show();
     }
 
+    /**
+     * 显示提示信息
+     *
+     * @param message
+     */
     private void show(String message) {
         Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
 //        toast.setGravity(Gravity.TOP,0,0);
